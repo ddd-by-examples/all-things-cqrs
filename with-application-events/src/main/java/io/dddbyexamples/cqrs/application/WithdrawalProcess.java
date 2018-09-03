6570fe002f8521f64c1af65a6436301755578c64
Jakub Pilimon <jakub.pilimon@gmail.com>
2018-09-03 13:38:05 +0200

    added synchronization with spring application events

---
+++ with-application-events/src/main/java/io/dddbyexamples/cqrs/application/WithdrawalProcess.java
@@ -0,0 +1,31 @@
+package io.dddbyexamples.cqrs.application;
+
+import io.dddbyexamples.cqrs.model.CardWithdrawn;
+import io.dddbyexamples.cqrs.model.CreditCard;
+import io.dddbyexamples.cqrs.persistance.CreditCardRepository;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Service
+public class WithdrawalProcess {
+
+    private final CreditCardRepository creditCardRepository;
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    WithdrawalProcess(CreditCardRepository creditCardRepository, ApplicationEventPublisher applicationEventPublisher) {
+        this.creditCardRepository = creditCardRepository;
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+
+    @Transactional
+    public void withdraw(UUID cardId, BigDecimal amount) {
+        CreditCard creditCard = creditCardRepository.findById(cardId)
+                .orElseThrow(() -> new IllegalStateException("Cannot find card with id " + cardId));
+        CardWithdrawn event = creditCard.withdraw(amount);
+        applicationEventPublisher.publishEvent(event);
+    }
+}
